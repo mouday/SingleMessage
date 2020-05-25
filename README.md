82fdde7eefadf5eb24acdbf4d042f7c776aec8dc
pengshiyu <1940607002@qq.com>
2020-05-25 14:49:37 +0800

    first

---
+++ README.md
@@ -0,0 +1,38 @@
+# single-message
+
+基于 Vue 和 ElementUI，限制只有一个消息提示
+
+使用方式
+
+引入
+
+```js
+import Vue from "vue";
+import SingleMessage from "single-message";
+
+Vue.prototype.$msg = Message;
+```
+
+vue 文件中中调用
+
+```js
+// 完整写法
+this.$msg.message({
+  message: "成功",
+  type: "成功",
+});
+
+// 快捷写法
+this.$msg.success("成功");
+this.$msg.warning("提醒");
+this.$msg.info("信息");
+this.$msg.error("错误");
+```
+
+js 文件中调用
+
+```js
+import SingleMessage from "single-message";
+
+SingleMessage.success("成功");
+```
---
+++ index.js
@@ -0,0 +1,45 @@
+import { Message } from "element-ui";
+
+class SingleMessage {
+  static message(options) {
+    // 判断是否已经弹框
+    if (document.getElementsByClassName("el-message").length === 0) {
+      Message(options);
+    }
+  }
+
+  static _message(message, type, options = null) {
+    let _options = {
+      message: message,
+      type: type,
+    };
+
+    if (options) {
+      _options = Object.assign(_options, options);
+    }
+
+    this.message(_options);
+  }
+
+  static success(message, options = null) {
+    this._message(message, "success", options);
+  }
+
+  static warning(message, options = null) {
+    this._message(message, "warning", options);
+  }
+
+  static info(message) {
+    this._message(message, "info", options);
+  }
+
+  static error(message) {
+    this._message(message, "error", options);
+  }
+
+  static close() {
+    Message.close();
+  }
+}
+
+export default SingleMessage;
